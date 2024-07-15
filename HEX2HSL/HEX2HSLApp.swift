0836c412f640d68b3107a54ac4f59ef19fe3a676
Wayne Dahlberg <waynefj40@gmail.com>
2024-07-15 10:25:10 -0600

    Initial Commit

---
+++ HEX2HSL/HEX2HSLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HEX2HSLApp.swift
+//  HEX2HSL
+//
+//  Created by Wayne Dahlberg on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HEX2HSLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
